229fd273f93984a85eafc09dbc600e26cc0efc67
parthyadav3105 <parthyadav3105@gmail.com>
2019-04-12 00:27:25 +0530

    signed-of-by: Parth yadav parthyadav3105@gmail.com \n day 1 commit \n status: did some work on home page

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+
+<head>
+
+	<meta charset="UTF-8">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+
+	<meta name="keywords" content="Turington, Ramanujan College, Delhi University, Coding, PHP, C++, javascript, JAVA, Python, Ramanujan, DU, Software Development, Cell, Antha Prerna Cell, DU_CS, University of Delhi, website, app, Delhi, India ">
+
+	<meta name="title" content="Antha Prerna Cell">
+	<meta name="description" content="">
+	<meta name="author" href="https://github.com/parthyadav3105/" content="Parth yadav">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://turington.in">
+	<meta property="og:site_name" content="Antha Prerna Cell">
+	<meta property="og:title" content="Antha Prerna Cell">
+	<meta property="og:description" content="The annual technical fest of Ramanujan College, Delhi University.">
+	<meta property="og:type" content="website">
+	<meta property="og:image" itemprop="image" content="https://faisalakhtar.in/Turington19/favicon.png">
+	<meta property="og:site_name" content="Turington 2k19">
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:url" content="https://turington.in">
+	<meta name="twitter:site_name" content="Turington 2k19">
+	<meta name="twitter:title" content="Turington 2k19">
+	<meta name="twitter:description" content="The annual technical fest of Department of Computer Science, Ramanujan College, Delhi University.">
+	<meta name="twitter:type" content="website">
+	<meta name="twitter:image" itemprop="image" content="https://faisalakhtar.in/Turington19/favicon.png">
+	<meta name="twitter:image:alt" content="Turington 2k19 logo">
+	
+	<title>TuringTon 2k19</title>
+
+
+	<!-- Logo on tab -->
+	<link rel="icon" href="img/turington logo.jpeg">
+	
+	<!-- Add social media icon library -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<!--our css files-->
+	<link rel="stylesheet" href="stylesheets/index.css">
+	<link rel="stylesheet" href="stylesheets/home.css">
+	
+	<!--google fonts-->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<script type="text/javascript" src="data.js"></script>
+
+</head>
+
+
+<body>
+
+	<nav>
+		
+	</nav>
+
+	<div class="home-main home-box-blue home-pad20">
+		<div>
+			<img src="assets/logo.svg">
+		</div>
+		<div>
+			<h1>Antha Preena Cell</h1>
+			<strong>Software Development Cell of Ramanujan College</strong>
+		</div>
+	</div>
+
+	<div class="home-container-1 ">
+		<div class="home-container-1-box home-box-orange home-pad20">
+				<h2>Want an App/Website for your Business?</h2>
+		</div>
+		<div class="home-container-1-box home-box-purple home-pad20">
+				<h2>We Code cool stuff for you</h2>
+		</div>
+		<div class="home-container-1-box home-box-green home-pad20">
+				<h2>With Latest Technologies</h2>
+		</div>
+	</div>
+
+	<div class="home-container-2 home-apply home-pad20">
+			<h3>How to Apply?</h3>
+			Mail us at abc@gmail.com with your contact details and attach a pdf with <a href="assets/proposal.pdf">this</a> format containing details of your desired product.
+	</div>
+
+	<div class="home-container-3 home-quotes home-pad20">
+			<h3>Quotes</h3>
+			<div>
+				<div>
+
+
+					
+				</div>
+			</div>
+	</div>
+
+	<footer>
+		
+	</footer>
+	
+</body>
+
+</html>
